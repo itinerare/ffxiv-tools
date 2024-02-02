17cf3181f4ec557466989e672d280a7f4fdca131
itinerare <ad@itinerare.net>
2024-02-02 11:51:19 -0500

    fix: store hash in untracked file
    - dodges issues with exec() in prod environments
    - use filemtime() for date

--- config/version.php
+++ config/version.php
@@ -3,11 +3,14 @@
 // From https://gist.github.com/breadthe/ce6a815b295c485b4bd91c08a79e1bf2
 // Based on this tweet by @Xewl https://twitter.com/Xewl/status/1459219464369627144
 
-$hash = trim(exec('git log --pretty="%h" -n1 HEAD'));
-$date = Carbon\Carbon::parse(trim(exec('git log -n1 --pretty=%ci HEAD')));
+if (file_exists(base_path('version'))) {
+    $hash = file_get_contents(base_path('version'));
+    $date = Carbon\Carbon::parse(filemtime(base_path('version')));
+    $string = sprintf('v%s-%s', $date->format('y.m'), $hash);
+}
 
 return [
-    'date'   => $date,
-    'hash'   => $hash,
-    'string' => sprintf('v%s-%s', $date->format('y.m'), $hash),
+    'date'   => $date ?? 'unknown',
+    'hash'   => $hash ?? 'unknown',
+    'string' => $string ?? 'v??.??-unknown',
 ];
