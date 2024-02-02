5097ff920959d5013226e3d0f5211969b2efc428
itinerare <ad@itinerare.net>
2024-02-02 12:05:34 -0500

    fix: store version string in file, add creation to composer dump

--- config/version.php
+++ config/version.php
@@ -4,8 +4,8 @@
 // Based on this tweet by @Xewl https://twitter.com/Xewl/status/1459219464369627144
 
 if (file_exists(base_path('version'))) {
-    $hash = file_get_contents(base_path('version'));
-    $date = Carbon\Carbon::parse(filemtime(base_path('version')));
+    $hash = file(base_path('version'))[0];
+    $date = Carbon\Carbon::parse(file(base_path('version'))[1]);
     $string = sprintf('v%s-%s', $date->format('y.m'), $hash);
 }
 
