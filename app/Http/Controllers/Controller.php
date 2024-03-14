12a8ac75b1b446945dfc1f9d51519683075ec8b6
itinerare <ad@itinerare.net>
2024-03-14 11:16:27 -0400

    refactor: rebuild on Laravel 11 application skeleton

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,20 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
-
-class Controller extends BaseController {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    /**
-     * Show the index page.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function getIndex() {
-        return view('index');
-    }
+abstract class Controller
+{
+    //
 }
---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class IndexController extends Controller
+{
+    /**
+     * Show the index page.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function getIndex() {
+        return view('index');
+    }
+}
