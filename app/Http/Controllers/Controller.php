f8f3ff6eb2e6c78810757605f0d664ba811bc805
itinerare <ad@itinerare.net>
2024-04-05 08:22:27 -0400

    feat: set up flashing validation errors

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Session;
+
 abstract class Controller {
-    //
+    /**
+     * Create a new controller instance.
+     */
+    public function __construct() {
+        // Flash any errors
+        if (Session::get('errors')) {
+            foreach (Session::get('errors')->all() as $message) {
+                flash($message)->error();
+            }
+        }
+    }
 }
