f46187aab82d7392977f7454319874f6dc408100
itinerare <ad@itinerare.net>
2024-07-27 11:20:52 -0400

    feat: move price display to unified widget - use for diadem page

---
+++ resources/views/_item_price_display.blade.php
@@ -0,0 +1,18 @@
+@if (isset($displayHQ) && $displayHQ)
+    {{ isset($priceData->min_price_hq) ? number_format($priceData->min_price_hq) : '???' }} (HQ) /
+    {{ isset($priceData->min_price_nq) ? number_format($priceData->min_price_nq) : '???' }} (NQ)
+@else
+    {{ isset($priceData->min_price_nq) ? number_format($priceData->min_price_nq) : '???' }}
+@endif
+Gil<br />
+<small class="text-muted">
+    Sales per day: {{ isset($priceData->nq_sale_velocity) ? number_format($priceData->nq_sale_velocity) : '(No Data)' }}
+    @if (isset($displayHQ) && $displayHQ)
+        NQ / {{ isset($priceData->hq_sale_velocity) ? number_format($priceData->hq_sale_velocity) : '(No Data)' }} HQ
+    @endif
+     ・
+    @if ($priceData->uploadTime)
+        Last updated: {!! $priceData->uploadTime !!} ・
+    @endif
+    Last retrieved: {!! $priceData->updatedTime !!}
+</small>
--- resources/views/diadem/index.blade.php
+++ resources/views/diadem/index.blade.php
@@ -23,14 +23,8 @@
                         <ol>
                             @foreach ($rankedItems[$class] as $name => $item)
                                 <li>
-                                    <strong>{{ $name }}</strong> - {{ isset($item->min_price_nq) ? number_format($item->min_price_nq) : '???' }} Gil<br />
-                                    <small class="text-muted">
-                                        Sales per day: {{ isset($item->nq_sale_velocity) ? number_format($item->nq_sale_velocity) : '(No Data)' }} ・
-                                        @if ($item->uploadTime)
-                                            Last updated: {!! $item->uploadTime !!} ・
-                                        @endif
-                                        Last retrieved: {!! $item->updatedTime !!}
-                                    </small>
+                                    <strong>{{ $name }}</strong> -
+                                    @include('_item_price_display', ['priceData' => $item])
                                 </li>
                             @endforeach
                         </ol>
@@ -39,14 +33,8 @@
                                 <ul>
                                     @foreach ($node as $name => $item)
                                         <li>
-                                            <strong>{{ $name }}</strong> - {{ isset($item->min_price_nq) ? number_format($item->min_price_nq) : '???' }} Gil<br />
-                                            <small class="text-muted">
-                                                Sales per day: {{ isset($item->nq_sale_velocity) ? number_format($item->nq_sale_velocity) : '(No Data)' }} ・
-                                                @if ($item->uploadTime)
-                                                    Last updated: {!! $item->uploadTime !!} ・
-                                                @endif
-                                                Last retrieved: {!! $item->updatedTime !!}
-                                            </small>
+                                            <strong>{{ $name }}</strong> -
+                                            @include('_item_price_display', ['priceData' => $item])
                                         </li>
                                     @endforeach
                                 </ul>
