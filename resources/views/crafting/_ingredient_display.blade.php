ea9328d80e571153ad2696dae129e8e89a1db672
itinerare <ad@itinerare.net>
2024-07-30 14:56:26 -0400

    fix(crafting): better check if game item exists before getting shop data

--- resources/views/crafting/_ingredient_display.blade.php
+++ resources/views/crafting/_ingredient_display.blade.php
@@ -1,7 +1,7 @@
 <li>
     <strong>
         {{ $ingredient['amount'] }} {{ $ingredient['gameItem']->name ?? 'Unknown Item' }}
-        @if ($ingredient['gameItem']->shop_data)
+        @if ($ingredient['gameItem']?->shop_data)
             <span class="text-primary" data-bs-toggle="tooltip" data-toggle="tooltip"
                 title="Available from a vendor{{ $ingredient['gameItem']->shop_data['currency'] == 1 ? ' for ' . number_format($ingredient['gameItem']->shop_data['cost']) . ' Gil per' : '' }}">*</span>
         @endif
