fb831313a67d03600b698b46c7dbe21a57ee6002
itinerare <ad@itinerare.net>
2024-07-30 11:49:23 -0400

    feat(crafting): display items available from a vendor in ingredient list

--- resources/views/crafting/_ingredient_display.blade.php
+++ resources/views/crafting/_ingredient_display.blade.php
@@ -1,5 +1,11 @@
 <li>
-    <strong>{{ $ingredient['amount'] }} {{ $ingredient['gameItem']->name ?? 'Unknown Item' }}</strong> -
+    <strong>
+        {{ $ingredient['amount'] }} {{ $ingredient['gameItem']->name ?? 'Unknown Item' }}
+        @if ($ingredient['gameItem']->shop_data)
+            <span class="text-primary" data-bs-toggle="tooltip" data-toggle="tooltip"
+                title="Available from a vendor{{ $ingredient['gameItem']->shop_data['currency'] == 1 ? ' for ' . number_format($ingredient['gameItem']->shop_data['cost']) . ' Gil per' : '' }}">*</span>
+        @endif
+    </strong> -
     @include('_item_price_display', ['priceData' => $ingredient['priceData'], 'displayHQ' => $ingredient['recipe'] ? $recipe->can_hq : false])
     @if ($ingredient['recipe'])
         <br />
