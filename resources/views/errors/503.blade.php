54968484630a432d6628ee9690a8b1152da6cf35
itinerare <ad@itinerare.net>
2024-10-09 10:43:38 -0400

    chore(workflows): pre-render 503 view when deploying

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    The site is currently down for maintenance while an update is deployed; please stand by!
+    <p class="text-center">The site is currently down for maintenance while an update is deployed. Please stand by!</p>
 @endsection
