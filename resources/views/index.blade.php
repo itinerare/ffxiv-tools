3e14cb36b7cfc0587af00f367011b4d951439e2e
itinerare <ad@itinerare.net>
2025-01-08 16:20:06 -0500

    chore: add extended index text - add kofi button

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,15 @@
         Please select one of the tools from the navigation above!
     </p>
 
+    <p class="text-center">
+        If you encounter an issue while using the site, please create an issue on <a href="https://github.com/itinerare/ffxiv-tools/issues">GitHub</a> (preferred) or send me an email at <code>ffxiv [at] itinerare.net</code>.
+    </p>
+
+    <p class="text-center">
+        Additionally, if any of the utilities here are useful to you, consider donating via Ko-fi!<br />
+        <a href='https://ko-fi.com/A86644L3' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi5.png?v=6' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+    </p>
+
     <div class="accordion mb-3" id="changelogContainer">
         <div class="accordion-item bg-light-subtle border-0">
             <h2 class="accordion-header">
